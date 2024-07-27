4f7de83579941df7eed05805a156e72c63c23806
GCBallesteros <dev+github@maxwellrules.com>
2024-07-27 16:15:40 +0200

    A debug mode that gives you superpowers when you most need them

---
+++ debug_mode/__init__.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import os
+
+if "DBG" in os.environ:
+    import icecream
+    import wat
+    from ipdb import iex
+
+    icecream.ic.configureOutput(includeContext=True)
+
+    # Make ipdb the default debugger
+    os.environ["PYTHONBREAKPOINT"] = "ipdb.set_trace"
+
+    debug_folder = Path("./_debug")
+    debug_folder.mkdir(exist_ok=True)
+
+    icecream.install()
+
+    # Add a few things to builtins so that they are
+    # always available
+    builtins = __import__("builtins")
+
+    setattr(builtins, "wat", wat)
+    setattr(builtins, "iex", iex)
+    setattr(builtins, "debug_folder", debug_folder)
